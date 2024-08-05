5157a437b4bae653f32ab412277518593a32bbf7
DESKTOP-D2LSKGO\Arbaaz <bharat.s@adroitoutsourcing.com>
2024-08-05 18:51:06 +0530

    feature added

---
+++ HR.LeaveManagement.Application/Features/LeaveAllocation/Handler/Queries/GetLeaveAllocationListRequestHandler.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using HR.LeaveManagement.Application.DTOs;
+using HR.LeaveManagement.Application.Features.LeaveAllocation.Request.Queries;
+using HR.LeaveManagement.Application.Persistence.Contracts;
+using HR.LeaveManagement.Domain;
+using MediatR;
+
+namespace HR.LeaveManagement.Application.Features.LeaveAllocation.Handler.Queries
+{
+    public class GetLeaveAllocationListRequestHandler : IRequestHandler<GetLeaveAllocationListRequest, List<LeaveAllocationDto>>
+    {
+        
+        private readonly ILeaveAllocationRepository _leaveAllocationRepository;
+        private readonly IMapper _mapper;
+
+        public GetLeaveAllocationListRequestHandler(ILeaveAllocationRepository leaveAllocationRepository,
+                                                    IMapper mapper)
+        {
+            _leaveAllocationRepository = leaveAllocationRepository;
+            _mapper = mapper;
+        }
+        public async Task<List<LeaveAllocationDto>> Handle(GetLeaveAllocationListRequest request, CancellationToken cancellationToken)
+        {
+            var leaveAllocation = await _leaveAllocationRepository.GetAll();
+            return _mapper.Map<List<LeaveAllocationDto>>(leaveAllocation);
+        }
+
+  
+    }
+}
